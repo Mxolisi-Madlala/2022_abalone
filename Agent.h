c496d80846a0d2add4d3615ab79c8416eb989a74
Mxolisi-Madlala <213513391@stu.ukzn.ac.za>
2022-05-18 15:35:11 +0200

    master

---
+++ Agent.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <algorithm>
+#include <atomic>
+#include <tuple>
+#include "board.h"
+#include "cell.h"
+using namespace std;
+
+class Agent : board, cell{
+
+};
--- main.cpp
+++ main.cpp
@@ -23,29 +23,67 @@ using namespace std;
  * 
  */
 int main(int argc, char** argv) {
-    if(argc==2){
-        board abalone;
-        string ibs(abalone);
-        cout<<"Initial Abalone board state:"<<endl;
-        cout<<ibs<<endl;        
-        ifstream log(argv[1]);//sequence of game moves
-        string l;
-        int n, fd, md;
-        while(log>>l>>n>>fd>>md){
-            cout<<"Move: "<<l<<" "<<n<<" "<<fd<<" "<<md;
-            
-            
-            if(abalone.executeMove(l,n, fd, md)){
-                string nbs(abalone);
-                cout<<" - Next Abalone board state:"<<endl;
-                cout<<nbs<<endl;
-            }
-            else{
-                cout<<" - Invalid move!"<<endl;
-            }
-        }    
-        return 0;
-    }
-    return -1;
-}
 
+    int userSelect;
+    cout << "choose your marbles " <<endl;
+    cout << "Enter 1 for 0 and 2 for @ marbles: ";
+    cin >> userSelect;
+    switch (userSelect)
+    {
+    case 1: 
+        if(argc==2){
+            board abalone;
+            string ibs(abalone);
+            cout<<"Initial Abalone board state:"<<endl;
+            cout<<ibs<<endl;        
+            ifstream log(argv[1]);//sequence of game moves
+            string l;
+            int n, fd, md;
+            while(log>>l>>n>>fd>>md){
+                cout<<"Move: "<<l<<" "<<n<<" "<<fd<<" "<<md;
+                
+                
+                if(abalone.executeMove(l,n, fd, md)){
+                    string nbs(abalone);
+                    cout<<" - Next Abalone board state:"<<endl;
+                    cout<<nbs<<endl;
+                }
+                else{
+                    cout<<" - Invalid move!"<<endl;
+                }
+            }    
+            return 0;
+        }
+        return -1;
+    break;
+
+    case 2:
+        if(argc==2){
+            board abalone;
+            string ibs(abalone);
+            cout<<"Initial Abalone board state:"<<endl;
+            cout<<ibs<<endl;        
+            ifstream log(argv[1]);//sequence of game moves
+            string l;
+            int n, fd, md;
+            while(log>>l>>n>>fd>>md){
+                cout<<"Move: "<<l<<" "<<n<<" "<<fd<<" "<<md;
+                
+                
+                if(abalone.executeMove(l,n, fd, md)){
+                    string nbs(abalone);
+                    cout<<" - Next Abalone board state:"<<endl;
+                    cout<<nbs<<endl;
+                }
+                else{
+                    cout<<" - Invalid move!"<<endl;
+                }
+            }    
+            return 0;
+        }
+        return -1;
+        break;
+    default:
+        cout << "user score: ";
+    }
+}
